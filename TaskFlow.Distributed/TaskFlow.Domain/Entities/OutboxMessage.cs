24719ce395e8c017b08d28e19a873d0444d56dd6
Deepak Nangia <dknangia@gmail.com>
2026-04-20 18:45:37 -0400

    Outbox message domain EF entity

---
+++ TaskFlow.Distributed/TaskFlow.Domain/Entities/OutboxMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFlow.Domain.Entities
+{
+    public class OutboxMessage
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; } = string.Empty; // e.g., "TaskCreated"
+        public string Content { get; set; } = string.Empty; // JSON of the message
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? ProcessedAt { get; set; } // When it was sent to RabbitMQ
+    }
+}
