d0f48f43d674a4b40819be7ad02d2677965ac88d
Deepak Nangia <dknangia@gmail.com>
2026-04-20 18:45:55 -0400

    WorkTask EF Entity

---
+++ TaskFlow.Distributed/TaskFlow.Domain/Entities/WorkTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskFlow.Domain.Enums;
+using TaskStatus = TaskFlow.Domain.Enums.TaskStatus;
+
+namespace TaskFlow.Domain.Entities
+{
+    public class WorkTask
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;    
+        public string Payload { get; set; } = string.Empty;
+        public TaskStatus Status { get; set; } = TaskStatus.Pending;
+        public DateTime ScheduledFor { get; set; }
+        public int RetryCount { get; set; }
+
+        //Audit fields
+        public  DateTime CreatedAd { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+
+    }
+}
